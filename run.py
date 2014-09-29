19e0476d224c25f6442e5a02a6da6383eed4dd5d
Morag Scrimgeour <morag.astro@gmail.com>
2014-09-29 11:04:20 -0700

    Modified run.py for AWS'

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 #!/opt/local/bin/python
+from flask import Flask
 from app import app
-app.run(debug = True)
-#if __name__ == "__main__":
-#    app.run(debug=True, host='0.0.0.0', port=80)
+#app.run(debug = True)
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0', port=80)
